3bb896a0e68706ca13c6c8f868a392d2d7701493
web-padawan <iamkulykov@gmail.com>
2023-10-26 16:39:50 +0300

    Update project: add grid and sync selected items

--- src/main/java/com/example/application/views/home/HomeView.java
+++ src/main/java/com/example/application/views/home/HomeView.java
@@ -1,14 +1,19 @@
 package com.example.application.views.home;
 
 import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.data.provider.ListDataView;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.NativeButton;
 import com.vaadin.flow.component.combobox.MultiSelectComboBox;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.splitlayout.SplitLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+
+import java.util.Set;
 
 import com.example.application.domain.Country;
 import com.example.application.domain.DataService;
@@ -19,36 +24,44 @@ import com.example.application.domain.DataService;
 public class HomeView extends Composite<VerticalLayout> {
 
     public HomeView() {
+        getContent().setHeightFull();
+        getContent().setWidthFull();
+
+        Grid<Country> grid = new Grid<>(Country.class, false);
+        grid.setItems(DataService.getCountries());
+        grid.setSelectionMode(Grid.SelectionMode.MULTI);
+        grid.setMinWidth("300px");
+        grid.addColumn(Country::getName).setHeader("Name");
+        grid.addColumn(Country::getAbbreviation).setHeader("Abbr");
+
         MultiSelectComboBox<Country> comboBox = new MultiSelectComboBox<>(
                 "Countries");
         comboBox.setItems(DataService.getCountries());
         comboBox.setItemLabelGenerator(Country::getName);
 
-        getContent().setHeightFull();
-        getContent().setWidthFull();
-        getContent().add(comboBox);
+        grid.addSelectionListener(event -> {
+            if (event.isFromClient()) {
+                Set<Country> addedSelection = event.getAllSelectedItems();
+                comboBox.deselectAll();
+                comboBox.select(addedSelection);
+            }
+        });
 
-        NativeButton groupButton = new NativeButton("Group selected items",
-                event -> {
-                    comboBox.setGroupSelectedItems(true);
+        comboBox.addSelectionListener(event -> {
+            if (event.isFromClient()) {
+                Set<Country> addedSelection = event.getAllSelectedItems();
+                grid.deselectAll();
+                addedSelection.stream().forEach(item -> {
+                    grid.select(item);
                 });
+            }
+        });
 
-        NativeButton ungroupButton = new NativeButton("Un-group selected items",
-                event -> {
-                    comboBox.setGroupSelectedItems(false);
-                });
+        HorizontalLayout layout = new HorizontalLayout(comboBox);
+        layout.setPadding(true);
 
-        NativeButton showAllChips = new NativeButton("Show all chips",
-                event -> {
-                    comboBox.setAllChipsVisible(true);
-                });
-
-        NativeButton dontShowAllChips = new NativeButton("Don't show all chips",
-                event -> {
-                    comboBox.setAllChipsVisible(false);
-                });
-
-        getContent().add(new HorizontalLayout(groupButton, ungroupButton,
-                showAllChips, dontShowAllChips));
+        SplitLayout splitLayout = new SplitLayout(grid, layout);
+        splitLayout.setWidthFull();
+        getContent().add(splitLayout);
     }
 }
